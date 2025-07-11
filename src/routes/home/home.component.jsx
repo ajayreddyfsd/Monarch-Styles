aa245ad41d358407e150dd43f7fbb1a89049e4e4
Ajay Reddy <ajayreddy.fsd@gmail.com>
2025-07-11 11:10:58 -0400

    project starter kit ready

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,42 +1,48 @@
-import { Outlet } from 'react-router-dom';
-
-import Directory from '../../components/directory/directory.component';
+import Directory from "../../components/directory/directory.component";
 
 const Home = () => {
   const categories = [
     {
       id: 1,
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
       id: 2,
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
       id: 3,
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
       id: 4,
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
       id: 5,
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
 
   return (
     <div>
       <Directory categories={categories} />
-      <Outlet />
     </div>
   );
 };
 
 export default Home;
+
+// what is this home component actually doing??????
+
+// we have a created an array of objs. each obj has imageURL for the category, title for the category, and id for the category.
+// so all of this as a bunch we are passing it to directory component through props
+// in the directory component, all it does is to create an {image and title} box for each of those objs
+// so it uses map and creates category-comp for each by passing their respective imageURL and title as prop
+// now finally, each of those category-comps, go to their respective category-comp code along with their respective
+// imageURL and title and produces a block as output which we see on the screen
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,22 +1,29 @@
-import { Fragment } from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
 
-import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
-import './navigation.styles.scss';
+import "./navigation.styles.scss";
 
 const Navigation = () => {
   return (
+    //these are just a group of links
+    //first one is an image based link for the logo
+    //second and third are the usual internal navigation links
+    //we are using Link tag instead of a-tag as that is recommended in react for smooth internal transitions
+    //finally we have put outlet tag, coz we need this component on all the webpages.
+    //so we use this component as nested route and we need to tell the children routes
+    //where they are supposed to go in the parent route
     <Fragment>
-      <div className='navigation'>
-        <Link className='logo-container' to='/'>
-          <CrwnLogo className='logo' />
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
         </Link>
-        <div className='nav-links-container'>
-          <Link className='nav-link' to='/shop'>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
             SHOP
           </Link>
-          <Link className='nav-link' to='/sign-in'>
+          <Link className="nav-link" to="/sign-in">
             SIGN IN
           </Link>
         </div>
