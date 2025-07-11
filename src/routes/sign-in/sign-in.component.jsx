803b852fcf8da3df816f4ab0b1f5eab999b15aa5
Ajay Reddy <ajayreddy.fsd@gmail.com>
2025-07-11 15:05:05 -0400

    successfully stored data of the user that signed in through google

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,18 +1,26 @@
-// import {
-//   signInWithGooglePopup,
-//   createUserProfileDocument,
-// } from "../../utils/firebase/firebase.utils";
+// remember we wrote google sign and auth in firebase utils, we are just importing them into this signin component
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
 
+//now before we start using the firebase utils
+//go to the firebase project
+//go to authentication tab, then sign-in method, choose the provider, here google, and save
+//also give a support email
+//u can also perform 2 factor sms authentication, but need to upgrade the firebase project
 const SignIn = () => {
-  // const logGoogleUser = async () => {
-  //   const response = await signInWithGooglePopup();
-  //   createUserProfileDocument(response);
-  // };
+  //simple function that waits till promise is resolved and prints the response to the console
+  //u can see the response and access token in the google dev tab console
+  const logGoogleUser = async () => {
+    const {user} = await signInWithGooglePopup();
+    createUserDocumentFromAuth(user);
+  };
 
   return (
     <div>
       <h1>Sign In Page</h1>
-      {/* <button>Sign in with Google Popup</button> */}
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
     </div>
   );
 };
