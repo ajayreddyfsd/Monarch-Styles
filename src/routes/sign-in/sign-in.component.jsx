61f93dfe4928e05656490fd219dcecb79d3f95bc
Ajay Reddy <ajayreddy.fsd@gmail.com>
2025-07-18 18:57:27 -0400

    phase-1 complete

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,33 +1,109 @@
-// remember we wrote google sign and auth in firebase utils, we are just importing them into this signin component
+import { useState, useContext } from "react";
+
+import FormInput from "../../components/form-input/form-input.component";
+import Button, {
+  BUTTON_TYPE_CLASSES,
+} from "../../components/button/button.component";
+import { UserContext } from "../../contexts/user.context";
+
 import {
+  signInAuthUserWithEmailAndPassword,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
-//now before we start using the firebase utils
-//go to the firebase project
-//go to authentication tab, then sign-in method, choose the provider, here google, and save
-//also give a support email
-//u can also perform 2 factor sms authentication, but need to upgrade the firebase project
-const SignIn = () => {
-  //for popup
-  //simple function that waits till promise is resolved and prints the response to the console
-  //u can see the response and access token in the google dev tab console
-  const logGoogleUser = async () => {
-    const response = await signInWithGooglePopup();
+import "./sign-in-form.styles.scss";
 
-    //i just need the user object from the whole resposne object
-    //which we will pass to the create user function
-    //this will be userAuth for the createUserDocumentFromAuth
-    const { user: userAuth } = response;
-    console.log("user or userAuth obj of response obj:", userAuth);
-    createUserDocumentFromAuth(userAuth);
+const defaultFormFields = {
+  email: "",
+  password: "",
+};
+
+const SignIn = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  const { setCurrentUser } = useContext(UserContext);
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const signInWithGoogle = async () => {
+    const { user } = await signInWithGooglePopup();
+
+    //storing the user data that signed in using google
+    await createUserDocumentFromAuth(user);
+    setCurrentUser(user);
+    resetFormFields();
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { user } = await signInAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      setCurrentUser(user);
+      resetFormFields();
+    } catch (error) {
+      switch (error.code) {
+        case "auth/wrong-password":
+          alert("incorrect password for email");
+          break;
+        case "auth/user-not-found":
+          alert("no user associated with this email");
+          break;
+        default:
+          console.log(error);
+      }
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields({ ...formFields, [name]: value });
   };
 
   return (
-    <div>
-      <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+    <div className="sign-in-container">
+      <h2>Already have an account?</h2>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Email"
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
+
+        <FormInput
+          label="Password"
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
+
+        {/* 2 options for the user to sign-in
+        normal sign-in and sign-in through google */}
+        <div className="buttons-container">
+          <Button type="submit">Sign In</Button>
+          <Button
+            buttonType={BUTTON_TYPE_CLASSES.google}
+            type="button"
+            onClick={signInWithGoogle}
+          >
+            Sign In With Google
+          </Button>
+        </div>
+      </form>
     </div>
   );
 };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -146,6 +146,9 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
   return await createUserWithEmailAndPassword(auth, email, password);
 };
 
+
+
+//this is for sign-ins
 export const signInAuthUserWithEmailAndPassword = async (email, password) => {
   if (!email || !password) return;
 
